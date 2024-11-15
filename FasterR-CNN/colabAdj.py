3ba6699a302a5a8ae3b981af415b188e0610e679
Stuart <stustu1389@gmail.com>
2024-11-15 18:41:49 +0000

    Test google colab dir implementation

--- FasterR-CNN/colabAdj.py
+++ FasterR-CNN/colabAdj.py
@@ -2,7 +2,7 @@ import os
 
 def checkColab():
     if "COLAB_GPU" in os.environ:
-        base_dir = "/content/drive/My Drive/Colab Notebooks/Thesis"
+        base_dir = "\\content\\drive\\My Drive\\Colab Notebooks\\Thesis"
     else:
         base_dir = "N:\\University subjects\\Thesis\\Python projects\\SmokeFasterRCNN"
     return base_dir
