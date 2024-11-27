293922cc55a2f107211fe339ac53045846454949
Stuart <stustu1389@gmail.com>
2024-11-27 20:42:57 +0000

    idk

--- FasterR-CNN/Plot_Graphs.py
+++ FasterR-CNN/Plot_Graphs.py
@@ -5,7 +5,7 @@ def plot_all_loss(train_dict, validate_dict):
     loss_types = train_dict[0].keys()
 
     # Creating subplots for each loss type
-    fig, axes = plt.subplots(1, len(loss_types), figsize=(20, 5), sharey=True)
+    fig, axes = plt.subplots(1, len(loss_types), figsize=(20, 5), sharey=False)
     fig.suptitle("Train vs Validate Loss")
 
     # Plot each loss type
@@ -15,7 +15,7 @@ def plot_all_loss(train_dict, validate_dict):
 
         # Plot on respective axis
         axes[i].plot(range(1, len(train_dict) + 1), train_losses, label='Train', marker='o')
-        axes[i].plot(range(1, len(validate_dict) + 1), validate_losses, label='Validate', marker='x')
+        axes[i].plot(range(1, len(validate_dict) + 1), validate_losses, label='Validate', marker='o', color="orange")
 
         axes[i].set_title(loss_type.replace('_', ' ').capitalize())
         axes[i].set_xlabel('Epochs')
