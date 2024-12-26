ea2f97c0f5f6ccc36f585ced5b84f87003e31441
Stuart <stustu1389@gmail.com>
2024-12-26 01:22:25 +0000

    inc lr l4

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -9,8 +9,8 @@ def setTrainValues(val_to_get):
         "EPOCHS": 25,
         "PATIENCE": 4,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data"
-        "model_name": "with_transform_L4", # name of saved model
-        "plotJSON_fname": "with_transform_L4", # json filename
+        "model_name": "inc_lr_L4", # name of saved model
+        "plotJSON_fname": "inc_lr_L4", # json filename
 
         "load_hd5f" : False, # whether to load from hd5f MAKE SURE THIS IS OFF WHEN CREATING HD5F
         "h5py_dir_save_name": "basic_transform_large", # file name for h5py file
@@ -21,7 +21,7 @@ def setTrainValues(val_to_get):
         "record_trace" : False,
 
         # PARAMETERS
-        "learning_rate": 0.001,
+        "learning_rate": 0.003,
         "momentum": 0.9,
         "weight_decay": 0.0005,
         "step_size": 3,
@@ -33,7 +33,7 @@ def setTrainValues(val_to_get):
 def setTestValues(val_to_get):
     # Create dictionary with  values
     test_values = {
-        "BATCH_SIZE": 4,
+        "BATCH_SIZE": 8,
         "dataset": "Large data", # "Small data" OR "Large data"
         "model_name": "with_transform_L4", # name of model to test
 
