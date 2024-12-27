3b68f879a15a053e7ae83f8b4d76bc0f1267be86
Stuart <stustu1389@gmail.com>
2024-12-27 15:10:49 +0000

    downgrade pytorch

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -9,8 +9,8 @@ def setTrainValues(val_to_get):
         "EPOCHS": 25,
         "PATIENCE": 4,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data"
-        "model_name": "with_debug_L4", # name of saved model
-        "plotJSON_fname": "with_debug_L4", # json filename
+        "model_name": "with_debug_L4_dg", # name of saved model
+        "plotJSON_fname": "with_debug_L4_dg", # json filename
 
         "load_hd5f" : False, # whether to load from hd5f MAKE SURE THIS IS OFF WHEN CREATING HD5F
         "h5py_dir_save_name": "test_file_large_compB", # file name for h5py file
