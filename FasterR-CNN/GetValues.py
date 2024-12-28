4397c160bdd19265e3d41caa8bd25dea03aee070
Stuart <stustu1389@gmail.com>
2024-12-28 20:20:14 +0000

    baseline colab T4

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -5,12 +5,12 @@ import torch
 def setTrainValues(val_to_get):
     # Creating dictionary with values
     train_values = {
-        "BATCH_SIZE": 16,
+        "BATCH_SIZE": 4,
         "EPOCHS": 15,
         "PATIENCE": 4,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data"
-        "model_name": "003_lr_A100", # name of saved model
-        "plotJSON_fname": "003_lr_A100", # json filename
+        "model_name": "BaselineColabT4", # name of saved model
+        "plotJSON_fname": "BaselineColabT4", # json filename
 
         "load_hd5f" : False, # whether to load from hd5f MAKE SURE THIS IS OFF WHEN CREATING HD5F
         "h5py_dir_save_name": "test_file_large_compB", # file name for h5py file
