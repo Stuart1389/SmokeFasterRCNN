02fd6080973f4c6bd530bca4053bd3f266b084f6
Stuart <stustu1389@gmail.com>
2024-12-29 16:36:49 +0000

    004 lr a100

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -5,13 +5,13 @@ import torch
 def setTrainValues(val_to_get):
     # Creating dictionary with values
     train_values = {
-        "BATCH_SIZE": 8,
+        "BATCH_SIZE": 16,
         "EPOCHS": 15,
         "PATIENCE": 4,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data"
-        "model_name": "004_lr_L4", # name of saved model
-        "plotJSON_fname": "004_lr_L4", # json filename
-        "model_id": "18",
+        "model_name": "004_lr_A100", # name of saved model
+        "plotJSON_fname": "004_lr_A100", # json filename
+        "model_id": "19",
 
         "load_hd5f" : False, # whether to load from hd5f MAKE SURE THIS IS OFF WHEN CREATING HD5F
         "h5py_dir_save_name": "test_file_large_compB", # file name for h5py file
