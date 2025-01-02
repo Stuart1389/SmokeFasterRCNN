59306e0b6acd4f8edb51337ddf12650572e15f8d
Stuart <stustu1389@gmail.com>
2025-01-02 23:37:49 +0000

    random crop

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -5,13 +5,13 @@ import torch
 def setTrainValues(val_to_get):
     # Creating dictionary with values
     train_values = {
-        "BATCH_SIZE": 16,
+        "BATCH_SIZE": 14,
         "EPOCHS": 15,
         "PATIENCE": 4,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data"
-        "model_name": "random_rotate_A100", # name of saved model
-        "plotJSON_fname": "random_rotate_A100", # json filename
-        "model_id": "25",
+        "model_name": "random_crop_A100", # name of saved model
+        "plotJSON_fname": "random_crop_A100", # json filename
+        "model_id": "26",
 
         "load_hd5f" : False, # whether to load from hd5f MAKE SURE THIS IS OFF WHEN CREATING HD5F
         "h5py_dir_save_name": "test_file", # file name for h5py file
