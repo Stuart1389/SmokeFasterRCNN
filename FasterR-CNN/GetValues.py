f07563bf8b8a8a5d7fc74c780458a521ddff23ef
Stuart <stustu1389@gmail.com>
2025-01-17 23:28:12 +0000

    train resnet34 fpnv2 8

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -5,7 +5,7 @@ import torch
 def setTrainValues(val_to_get):
     # Creating dictionary with values
     train_values = {
-        "BATCH_SIZE": 12,
+        "BATCH_SIZE": 8,
         "EPOCHS": 20,
         "PATIENCE": 5,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data", "Small data cloud"
