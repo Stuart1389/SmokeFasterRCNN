69a9ed17d3397148b5bfac4cbc6e155aee9431ad
Stuart <stustu1389@gmail.com>
2025-02-05 22:51:45 +0000

    a100 5lr

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -9,8 +9,8 @@ def setTrainValues(val_to_get):
         "EPOCHS": 15,
         "PATIENCE": 5,
         "dataset": "Large data", # "Small data" , "Medium data" OR "Large data", "Small data cloud"
-        "model_name": "transform_csj_ma_5s_a100", # name of saved model
-        "plotJSON_fname": "transform_csj_ma_5s_a100", # json filename
+        "model_name": "transform_csj_ma_5lr_a100", # name of saved model
+        "plotJSON_fname": "transform_csj_ma_5lr_a100", # json filename
         "model_id": "56",
         "save_at_end" : False,
         # temp resnet101_2080_101,
@@ -39,7 +39,7 @@ def setTrainValues(val_to_get):
         "logWB" : True,
 
         # PARAMETERS
-        "learning_rate": 0.003,
+        "learning_rate": 0.005,
         "momentum": 0.9,
         "weight_decay": 0.0005,
         "step_size": 5,
@@ -77,7 +77,7 @@ def setTestValues(val_to_get):
     test_values = {
         "BATCH_SIZE": 4,
         "dataset": "Large data", # "Small data" OR "Large data"
-        "model_name": "transform_csj_multi_anchor_a100", # name of model to test
+        "model_name": "transform_csj_ma_5s_a100", # name of model to test
 
         # PROFILER
         "start_profiler": False,
