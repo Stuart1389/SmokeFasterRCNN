ef0eb7a04cfe1062ae0dac221f9356632464ac1f
Stuart <stustu1389@gmail.com>
2025-02-06 10:47:07 +0000

    large data

--- FasterR-CNN/GetValues.py
+++ FasterR-CNN/GetValues.py
@@ -8,7 +8,7 @@ def setTrainValues(val_to_get):
         "BATCH_SIZE": 16,
         "EPOCHS": 20,
         "PATIENCE": 5,
-        "dataset": "Large_large", # "Small data" , "Medium data" OR "Large data", "Small data cloud"
+        "dataset": "Large data", # "Small data" , "Medium data" OR "Large data", "Small data cloud"
         "model_name": "transform_csj_ma_7s_a100", # name of saved model
         "plotJSON_fname": "transform_csj_ma_7s_a100", # json filename
         "model_id": "56",
